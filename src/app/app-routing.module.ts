aa45f73fb12a7661174af62376e353261e7204e7
TheBenitoo <benitograuel@googlemail.com>
2022-09-06 15:17:39 +0200

    add navigation & darkmode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImdbComponent } from './modules/api/imdb/imdb.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'imdb', component: ImdbComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
